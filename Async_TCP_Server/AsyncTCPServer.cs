c7be91b0a1f47b02b4a4737e699547e1d3730c89
Krzyszt000f <56268907+Krzyszt000f@users.noreply.github.com>
2020-11-08 21:49:22 +0100

    Async_TCP_Server

---
+++ Async_TCP_Server/AsyncTCPServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Async_TCP_Server
+{
+    class AsyncTCPServer
+    {
+        static void Main(string[] args)
+        {
+            BibliotekaKlas.AsyncSummingServer s = new BibliotekaKlas.AsyncSummingServer(IPAddress.Any, 2048);
+            s.Start();
+        }
+    }
+}
